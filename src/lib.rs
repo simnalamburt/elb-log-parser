4bc38cb29d645ff1d827d16adea510cea725b901
Hyeon Kim <simnalamburt@gmail.com>
2021-12-19 04:46:32 +0900

    Hello, LALRPOP!

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use lalrpop_util::lalrpop_mod;
+
+lalrpop_mod!(pub classic_load_balancer); // synthesized by LALRPOP
+
+#[test]
+fn test_classic_load_balancer() {
+    // TODO: Test Classic LB v1 and v2 log formats
+    assert!(classic_load_balancer::TermParser::new().parse("22").is_ok());
+    assert!(classic_load_balancer::TermParser::new().parse("(22)").is_ok());
+    assert!(classic_load_balancer::TermParser::new().parse("((((22))))").is_ok());
+    assert!(classic_load_balancer::TermParser::new().parse("((22)").is_err());
 }
