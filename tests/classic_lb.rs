dc73e6bd2e0860604864276a5f59cfaff2501d55
Hyeon Kim <simnalamburt@gmail.com>
2021-12-20 23:31:41 +0900

    Parse IPv4

--- tests/classic_lb.rs
+++ tests/classic_lb.rs
@@ -1,5 +1,5 @@
 // TODO: Test Classic LB v1 and v2 log formats
-use elb_log_parser::classic_lb::{TimestampParser, NameParser};
+use elb_log_parser::classic_lb::{TimestampParser, NameParser, IPParser};
 
 #[test]
 fn test_timestamp() {
@@ -19,4 +19,14 @@ fn test_name() {
     assert!(NameParser::new().parse("asd*asd").is_err());
     assert!(NameParser::new().parse("asd가asd").is_err());
     assert!(NameParser::new().parse("TOO-LONG-ELB-NAME-TOO-LONG-ELB-NAME-TOO-LONG-ELB-NAME-TOO-LONG-ELB-NAME-TOO-LONG-ELB-NAME").is_err());
+}
+
+#[test]
+fn test_ipv4() {
+    assert!(IPParser::new().parse("127.0.0.1").is_ok());
+    assert!(IPParser::new().parse("1.1.1.1").is_ok());
+    assert!(IPParser::new().parse("255.255.255.255").is_ok());
+
+    assert!(IPParser::new().parse("255.255.255.2555").is_err());
+    assert!(IPParser::new().parse("255.255.255").is_err());
 }
