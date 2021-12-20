f935435683da5761e1c54a9631da464b6d56a237
Hyeon Kim <simnalamburt@gmail.com>
2021-12-20 19:09:05 +0900

    Rename: classic_load_balancer -> classic_lb

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 use lalrpop_util::lalrpop_mod;
 
-lalrpop_mod!(pub classic_load_balancer); // synthesized by LALRPOP
+lalrpop_mod!(pub classic_lb); // synthesized by LALRPOP
 
 // TODO: Test Classic LB v1 and v2 log formats
 
 #[test]
 fn test_iso8601() {
-    assert!(classic_load_balancer::TimestampParser::new().parse("2021-11-22T01:58:01.532018Z").is_ok());
+    assert!(classic_lb::TimestampParser::new().parse("2021-11-22T01:58:01.532018Z").is_ok());
 }
