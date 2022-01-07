bb6985b0d7e9889a56d2bbcd332211a992f389a0
Hyeon Kim <simnalamburt@gmail.com>
2022-01-08 06:28:31 +0900

    Handle errors properly

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::cell::RefCell;
 use std::io::{stdin, stdout, BufRead, BufWriter, Write};
 
+use anyhow::Result;
 use regex::bytes::{CaptureLocations, Regex};
-use serde::{Serialize, Serializer};
+use serde::{ser, Serialize, Serializer};
 use serde_json::to_writer;
+use thiserror::Error;
 
 #[derive(Serialize)]
 struct Log<'a> {
@@ -47,9 +49,19 @@ struct Log<'a> {
     ssl_protocol: &'a [u8],
 }
 
-fn bytes_ser<S>(bytes: &[u8], serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
-    // TODO: Deal with unwrapping
-    serializer.serialize_str(std::str::from_utf8(bytes).unwrap())
+fn bytes_ser<S>(bytes: &[u8], serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let str = std::str::from_utf8(bytes)
+        .map_err(|_| ser::Error::custom("log contains invalid UTF-8 characters"))?;
+    serializer.serialize_str(str)
+}
+
+#[derive(Error, Debug)]
+enum ParseLogError {
+    #[error("Invalid log line: {0:?}")]
+    InvalidLogFormat(Vec<u8>),
 }
 
 struct Parser {
@@ -112,17 +124,18 @@ impl Parser {
         Self { regex, locs }
     }
 
-    fn parse<'input>(&self, log: &'input [u8]) -> Log<'input> {
+    fn parse<'input>(&self, log: &'input [u8]) -> Result<Log<'input>, ParseLogError> {
         let mut locs = self.locs.borrow_mut();
-        // TODO: Deal with unwrapping
-        self.regex.captures_read(&mut locs, log).unwrap();
+        self.regex
+            .captures_read(&mut locs, log)
+            .ok_or_else(|| ParseLogError::InvalidLogFormat(log.to_owned()))?;
 
         let s = |i| {
             let (start, end) = locs.get(i).unwrap();
             &log[start..end]
         };
 
-        Log {
+        Ok(Log {
             time: s(1),
             elb: s(2),
             client_ip: s(3),
@@ -142,11 +155,11 @@ impl Parser {
             user_agent: s(17),
             ssl_cipher: s(18),
             ssl_protocol: s(19),
-        }
+        })
     }
 }
 
-fn main() {
+fn main() -> Result<()> {
     let stdin = stdin();
     let mut stdin = stdin.lock();
 
@@ -157,45 +170,44 @@ fn main() {
     let stdout = stdout.lock();
     let mut stdout = BufWriter::new(stdout);
 
-    // TODO: Deal with unwrapping
-    while stdin.read_until(b'\n', &mut buffer).unwrap() > 0 {
-        let log = parser.parse(&buffer);
-        // TODO: Deal with unwrapping
-        to_writer(&mut stdout, &log).unwrap();
-        // TODO: Deal with unwrapping
-        stdout.write(b"\n").unwrap();
+    while stdin.read_until(b'\n', &mut buffer)? > 0 {
+        let log = parser.parse(&buffer)?;
+        to_writer(&mut stdout, &log)?;
+        stdout.write(b"\n")?;
         buffer.clear();
     }
+
+    Ok(())
 }
 
 #[test]
-fn test_parser() {
+fn test_parser() -> Result<()> {
     let parser = Parser::new();
-    let t = |input, expected| {
-        assert_eq!(
-            serde_json::to_string(&mut parser.parse(input)).unwrap(),
-            expected
-        )
+    let t = |input, expected| -> Result<()> {
+        assert_eq!(serde_json::to_string(&mut parser.parse(input)?)?, expected);
+        Ok(())
     };
 
     t(
         br#"2015-05-13T23:39:43.945958Z my-loadbalancer 192.168.131.39:2817 10.0.0.1:80 0.000073 0.001048 0.000057 200 200 0 29 "GET http://www.example.com:80/ HTTP/1.1" "curl/7.38.0" - -
 "#,
         r#"{"time":"2015-05-13T23:39:43.945958Z","elb":"my-loadbalancer","client_ip":"192.168.131.39","client_port":"2817","backend_ip":"10.0.0.1","backend_port":"80","request_processing_time":"0.000073","backend_processing_time":"0.001048","response_processing_time":"0.000057","elb_status_code":"200","backend_status_code":"200","received_bytes":"0","sent_bytes":"29","http_method":"GET","url":"http://www.example.com:80/","http_version":"HTTP/1.1","user_agent":"\"curl/7.38.0\"","ssl_cipher":"-","ssl_protocol":"-"}"#,
-    );
+    )?;
     t(
         br#"2015-05-13T23:39:43.945958Z my-loadbalancer 192.168.131.39:2817 10.0.0.1:80 0.000086 0.001048 0.001337 200 200 0 57 "GET https://www.example.com:443/ HTTP/1.1" "curl/7.38.0" DHE-RSA-AES128-SHA TLSv1.2
 "#,
         r#"{"time":"2015-05-13T23:39:43.945958Z","elb":"my-loadbalancer","client_ip":"192.168.131.39","client_port":"2817","backend_ip":"10.0.0.1","backend_port":"80","request_processing_time":"0.000086","backend_processing_time":"0.001048","response_processing_time":"0.001337","elb_status_code":"200","backend_status_code":"200","received_bytes":"0","sent_bytes":"57","http_method":"GET","url":"https://www.example.com:443/","http_version":"HTTP/1.1","user_agent":"\"curl/7.38.0\"","ssl_cipher":"DHE-RSA-AES128-SHA","ssl_protocol":"TLSv1.2"}"#,
-    );
+    )?;
     t(
         br#"2015-05-13T23:39:43.945958Z my-loadbalancer 192.168.131.39:2817 10.0.0.1:80 0.001069 0.000028 0.000041 - - 82 305 "- - - " "-" - -
 "#,
         r#"{"time":"2015-05-13T23:39:43.945958Z","elb":"my-loadbalancer","client_ip":"192.168.131.39","client_port":"2817","backend_ip":"10.0.0.1","backend_port":"80","request_processing_time":"0.001069","backend_processing_time":"0.000028","response_processing_time":"0.000041","elb_status_code":"-","backend_status_code":"-","received_bytes":"82","sent_bytes":"305","http_method":"-","url":"-","http_version":"- ","user_agent":"\"-\"","ssl_cipher":"-","ssl_protocol":"-"}"#,
-    );
+    )?;
     t(
         br#"2015-05-13T23:39:43.945958Z my-loadbalancer 192.168.131.39:2817 10.0.0.1:80 0.001065 0.000015 0.000023 - - 57 502 "- - - " "-" ECDHE-ECDSA-AES128-GCM-SHA256 TLSv1.2
 "#,
         r#"{"time":"2015-05-13T23:39:43.945958Z","elb":"my-loadbalancer","client_ip":"192.168.131.39","client_port":"2817","backend_ip":"10.0.0.1","backend_port":"80","request_processing_time":"0.001065","backend_processing_time":"0.000015","response_processing_time":"0.000023","elb_status_code":"-","backend_status_code":"-","received_bytes":"57","sent_bytes":"502","http_method":"-","url":"-","http_version":"- ","user_agent":"\"-\"","ssl_cipher":"ECDHE-ECDSA-AES128-GCM-SHA256","ssl_protocol":"TLSv1.2"}"#,
-    );
+    )?;
+
+    Ok(())
 }
