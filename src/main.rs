b72ee07324ee2f9cb41bef0aa7adfd961c930eb5
Kyle Park <kyle@chai.finance>
2022-11-11 21:01:16 +0900

    Implement a basic CLI interface

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,57 @@ mod alb;
 mod classic_lb;
 mod parse;
 
-use crate::classic_lb::LogParser;
+use crate::alb::LogParser as ALBLogParser;
+use crate::classic_lb::LogParser as ClassicLBLogParser;
+use anyhow::Result;
+use clap::{Parser, ValueEnum};
 use std::io::{stdin, stdout, BufRead, BufWriter, Write};
 
-use anyhow::Result;
+#[derive(Parser)]
+#[command(about)]
+struct Args {
+    /// Type of load balancer
+    #[arg(value_enum, short, long, default_value_t = Type::Alb)]
+    r#type: Type,
+}
+
+#[derive(ValueEnum, Clone)]
+enum Type {
+    Alb,
+    ClassicLb,
+}
 
 fn main() -> Result<()> {
+    let args = Args::parse();
     let stdin = stdin();
     let mut stdin = stdin.lock();
 
     let mut buffer = Vec::new();
-    let parser = LogParser::new();
 
     let stdout = stdout();
     let stdout = stdout.lock();
     let mut stdout = BufWriter::new(stdout);
 
-    while stdin.read_until(b'\n', &mut buffer)? > 0 {
-        let log = parser.parse(&buffer)?;
-        serde_json::to_writer(&mut stdout, &log)?;
-        stdout.write(b"\n")?;
-        buffer.clear();
-    }
+    match args.r#type {
+        Type::Alb => {
+            let parser = ALBLogParser::new();
+            while stdin.read_until(b'\n', &mut buffer)? > 0 {
+                let log = parser.parse(&buffer)?;
+                serde_json::to_writer(&mut stdout, &log)?;
+                stdout.write(b"\n")?;
+                buffer.clear();
+            }
+        }
+        Type::ClassicLb => {
+            let parser = ClassicLBLogParser::new();
+            while stdin.read_until(b'\n', &mut buffer)? > 0 {
+                let log = parser.parse(&buffer)?;
+                serde_json::to_writer(&mut stdout, &log)?;
+                stdout.write(b"\n")?;
+                buffer.clear();
+            }
+        }
+    };
 
     Ok(())
 }
