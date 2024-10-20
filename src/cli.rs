0f6edd0d22c6e8ed0ec3b864f945dceca37ebcda
Hyeon Kim <simnalamburt@gmail.com>
2024-10-21 00:26:15 +0900

    Separate cli.rs from main.rs

---
+++ src/cli.rs
@@ -0,0 +1,28 @@
+use clap::{Parser, ValueEnum};
+
+#[derive(Parser)]
+#[command(about, version, arg_required_else_help(true))]
+pub struct Args {
+    /// Type of load balancer.
+    #[arg(value_enum, short, long, default_value_t = Type::Alb)]
+    pub r#type: Type,
+
+    #[command(flatten)]
+    pub config: Config,
+
+    /// Path of directory containing load balancer logs. To read from stdin, use "-".
+    pub path: String,
+}
+
+#[derive(ValueEnum, Clone)]
+pub enum Type {
+    Alb,
+    ClassicLb,
+}
+
+#[derive(Parser, Clone, Copy)]
+pub struct Config {
+    /// Skip parsing errors.
+    #[arg(long)]
+    pub skip_parse_errors: bool,
+}
